975c05fd7426c213e827ced048a7fd374698d844
sierramclawhorn <sierramclawhorn@gmail.com>
2017-03-04 15:06:43 -0800

    Delete rspec

--- test/test_clinic_finder.rb
+++ test/test_clinic_finder.rb
@@ -5,11 +5,6 @@ class EverythingOkayAlarm < TestClass
     assert true
   end
 
- 	describe "#create_full_address" do
-   	it "takes collection of clinic info and returns array of complete clinic addresses" do
-        @clinic_addresses.class.should eq Array
-    end
-	end
 
 
 end
