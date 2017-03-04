70f6ad259e63df54b5da1ccc41ca7d841f6332e9
katherinebui <kathy.codes@gmail.com>
2017-03-04 14:23:57 -0800

    methods drafted for closest clinics

--- lib/clinic_finder.rb
+++ lib/clinic_finder.rb
@@ -6,6 +6,8 @@ module ClinicFinder
     puts 'hello world'
   end
 
+  # goal: finding the closest clinic to our patient
+
   # goal: convert each address object into coordinates
   # in: clinic collection (array of hashes)
   # for each clinic object: take street address, city, state (concat to make address)
@@ -14,21 +16,40 @@ module ClinicFinder
     # output: array of lat/long
 
   def create_full_address(clinic_collection)
-    clinic_addresses = []
+    @clinic_addresses = []
     clinic_collection.each do |clinic, info|
-      clinic_addresses << ["#{info[:street_address]}, #{info[:city]}, #{info[:state]}"]
+      @clinic_addresses << ["#{info[:street_address]}, #{info[:city]}, #{info[:state]}"]
     end
-    clinic_addresses
+    @clinic_addresses
    # returns an array of string formatted addresses for geocoder method
   end
 
-  # goal: finding the closest clinic to our patient
-  # in: patient address
-  # use geocoder
-  def find_closest_clinic(clinic_collection, patient_address, radius)
-    create_full_address(clinic_collection)
+  def clinics_coordinates_conversion(clinic_addresses)
+    @clinic_addresses.map do |address|
+      location = Geokit::Geocoders::GoogleGeocoder.geocode(address)
+      location = location.ll
+    end
+    @clinic_addresses
+    # array of arrays of lat/long
+  end
+
+  def patient_coordinates_conversion(patient_address)
+    @patient_location = Geokit::Geocoders::GoogleGeocoder.geocode(patient_address)
+    @patient_location = @patient_location.ll
+  end
+
+  def calculate_distance(patient_address)
+    @distances = []
+    # @clinic_addresses.each do |clinic_address|
+    #   distances << @patient_location.distance_to(clinic_address)
+    # end
+    @distances = @clinic_addresses.sort_by{|clinic| clinic.distance_to(@patient_location)}
+    # returns a list of sorted lat/long from patient address
+  end
 
 
+  def find_closest_clinics(patient_address)
+    @distances[0..2]
   end
 
 end
