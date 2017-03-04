8572505bab90e47e255f9bbf0f142ce083b4bfc0
katherinebui <kathy.codes@gmail.com>
2017-03-04 11:26:15 -0800

    create full address takes array of clinic objects and puts the address in correct format for geocoder

--- lib/clinic_finder.rb
+++ lib/clinic_finder.rb
@@ -5,4 +5,23 @@ module ClinicFinder
   def self.hello_world
     puts 'hello world'
   end
+
+  # goal: convert each address object into coordinates
+  # in: clinic collection (array of hashes)
+  # for each clinic object: take street address, city, state (concat to make address)
+    # output: 1001 Brodway Oakland, CA
+  # pass ^ geocoder to find lat/long
+    # output: array of lat/long
+
+  def create_full_address(clinic_collection)
+    clinic_addresses = []
+    clinic_collection.each do |clinic|
+      clinic_addresses << ["#{clinic[:street_address]}, #{clinic[:city]}, #{clinic[:state]}"]
+    end
+    clinic_addresses
+   # returns an array of string formatted addresses for geocoder method
+  end
+
+
 end
+
