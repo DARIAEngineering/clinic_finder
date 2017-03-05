23ff187e475c4c935e2ee6305bf8ef9c1e57f7a0
Sara Gilford <sara.gilford@gmail.com>
2017-03-05 15:04:29 -0800

    Fix formatting

--- test/test_clinic_finder.rb
+++ test/test_clinic_finder.rb
@@ -57,22 +57,7 @@ class TestClinicFinder < TestClass
   end
 
   def test_locate_cheapest_clinic_locates
-    clinic = {
-          'planned_parenthood_oakland' =>
-          {
-            'street_address' => '1001 Broadway',
-            'city' => 'Oakland',
-            'state' => 'CA',
-            'zip' => 94607,
-            'accepts_naf' => false,
-            'gestational_limit' => 139,
-            'costs_9wks' => 425,
-            'costs_12wks' => 475,
-            'costs_18wks' => 975,
-            'costs_24wks' => nil,
-            'costs_30wks' => nil
-          }
-        }
-    assert_equal clinic, @abortron.locate_cheapest_clinic(gestational_age: 100)[0]
+    clinic_result = {name: 'planned_parenthood_oakland', cost: 975}
+    assert_equal clinic_result, @abortron.locate_cheapest_clinic(gestational_age: 100)[0]
   end
 end
