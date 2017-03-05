b510bddf383f8ccb83a34fabb71fef5d9c388521
Sara Gilford <sara.gilford@gmail.com>
2017-03-04 17:19:29 -0800

    Working gestational tier method

--- lib/clinic_finder/gestation_helper.rb
+++ lib/clinic_finder/gestation_helper.rb
@@ -1,18 +1,33 @@
 class ClinicFinder
   class GestationHelper
-    attr_reader :gestational_age
+    attr_reader :gestational_age, :gestational_weeks
 
     def initialize(gestational_age)
       @gestational_age = gestational_age
+      @gestational_weeks = (@gestational_age/7.0).ceil
     end
 
-    def gestation_in_weeks
-      (@gestational_age/7.0).ceil
-    end
+    def gestational_tier(clinic_attributes)
+      gestational_hash = {}
 
-    def price_at_gestation(clinic_hash)
+      clinic_attributes.each do |key, value|
+        if key.to_s[/\d+/]
+          gestational_hash[(key.to_s[/\d+/])] = key
+        end
+      end
+
+      week_tiers = gestational_hash.keys.map(&:to_i)
+
+      week_tiers << @gestational_weeks
+      week_tiers.sort!
+
+      current_position = week_tiers.index(@gestational_weeks)
 
 
+      numerical_term = current_position == 0 ? week_tiers[1] : week_tiers[current_position - 1]
+
+      gestational_tier = gestational_hash[numerical_term.to_s]
+      return gestational_tier.to_s
     end
 
     def within_gestational_limit?(gestational_limit)
