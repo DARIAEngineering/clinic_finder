c7c4a16af973cd7abe4c81c0d7e2a87b70a653b2
sierramclawhorn <sierramclawhorn@gmail.com>
2017-03-04 20:54:30 -0800

    Convert coordinates to hash structure

--- lib/clinic_finder.rb
+++ lib/clinic_finder.rb
@@ -11,34 +11,39 @@ class ClinicFinder
 		@clinics = ::YAML.load_file(yml_file)
   end
 
-  def create_full_address(clinic_collection)
+  def create_full_address
     @clinic_addresses = []
-    clinic_collection.each do |clinic, info|
-      @clinic_addresses << ["#{info['street_address']}, #{info['city']}, #{info['state']}"]
+    @clinics.each do |clinic, info|
+      @clinic_addresses << {name: clinic, address: "#{info['street_address']}, #{info['city']}, #{info['state']}"}
     end
     @clinic_addresses
    # returns an array of string formatted addresses for geocoder method
   end
 
-  def clinics_coordinates_conversion(clinic_addresses)
-    @clinic_addresses = clinic_addresses
-    @clinic_addresses.map! do |address|
-      address = address.join 
-      location = ::Geokit::Geocoders::GoogleGeocoder.geocode(address)
-      location = location.ll
+  def clinics_coordinates_conversion
+    @coordinates_hash = {}
+    @clinic_addresses.map! do |address| # {name: 'Oakland Clinic', address: '101 Main St, Oakland, CA'}
+      # address = address.join 
+      location = ::Geokit::Geocoders::GoogleGeocoder.geocode(address[:address])
+      float_coordinates = location.ll.split(',').map(&:to_f)
+      @coordinates_hash[address[:name]] = float_coordinates
     end
-    @clinic_addresses
-    # array of strings of lat/long
+    @coordinates_hash
   end
+    # array of strings of lat/long
 
   def patient_coordinates_conversion(patient_zipcode)
     @patient_location = ::Geokit::Geocoders::GoogleGeocoder.geocode(patient_zipcode)
-    @patient_location = @patient_location.ll
+    @patient_location = @patient_location.ll.to_f
   end
 
   def calculate_distance
-    @distances = []
-    @distances = @clinic_addresses.sort_by{|clinic| clinic.distance_to(@patient_location)}
+    distances = []
+    @coordinates_hash.each do |name, coordinates|
+      distances << {name: name, distance: coordinates.distance_to(@patient_location)}
+      # distances = [ {name: "Oakland", distance: 2}, {name: "San Francisco", distance: 1} ]
+    end
+    @distances = distances.sort {|distance| distance[:distance]}
     # returns a list of sorted lat/long from patient address
   end
 
