347dbda2bd493ffafe38edf341c0a16a0da47972
sierramclawhorn <sierramclawhorn@gmail.com>
2017-03-04 19:16:22 -0800

    Create test for patient coordinate conversion; Change patient address to patient zip

--- lib/clinic_finder.rb
+++ lib/clinic_finder.rb
@@ -21,8 +21,9 @@ class ClinicFinder
   end
 
   def clinics_coordinates_conversion(clinic_addresses)
+    @clinic_addresses = clinic_addresses
     @clinic_addresses.map do |address|
-      address = address.join
+      address = address.join 
       location = ::Geokit::Geocoders::GoogleGeocoder.geocode(address)
       location = location.ll
     end
@@ -30,19 +31,19 @@ class ClinicFinder
     # array of arrays of lat/long
   end
 
-  def patient_coordinates_conversion(patient_address)
-    @patient_location = ::Geokit::Geocoders::GoogleGeocoder.geocode(patient_address)
+  def patient_coordinates_conversion(patient_zipcode)
+    @patient_location = ::Geokit::Geocoders::GoogleGeocoder.geocode(patient_zipcode)
     @patient_location = @patient_location.ll
   end
 
-  def calculate_distance(patient_address)
+  def calculate_distance
     @distances = []
     @distances = @clinic_addresses.sort_by{|clinic| clinic.distance_to(@patient_location)}
     # returns a list of sorted lat/long from patient address
   end
 
 
-  def find_closest_clinics(patient_address)
+  def find_closest_clinics
     @distances[0..2]
     # returns closest 3 clinics to the patient
   end
--- test/test_clinic_finder.rb
+++ test/test_clinic_finder.rb
@@ -37,29 +37,28 @@ class TestClinicFinder < TestClass
   	assert_equal [["1001 Broadway, Oakland, CA"], ["2430 Folsom, San Francisco, CA"], ["517 Castro, San Francisco, CA"], ["110 S Market, San Jose, CA"], ["570 Pacific, Monterey, CA"], ["1801 Mountain NW, Albuquerque, NM"], ["7473 Humboldt, Butte Meadows, CA"], ["2220 Tulare, Fresno, CA"], ["2025 Pacific, Los Angeles, CA"], ["3900 W Manchester, Los Angeles, CA"], ["5905 Wilshire, Los Angeles, CA"]], @abortron.create_full_address(@abortron.clinics)
   end
 
-  # def test_that_clinic_coordinates_are_array
-  # 	addresses = @abortron.create_full_address(@abortron.clinics)
-  # 	assert_kind_of Array, @abortron.clinics_coordinates_conversion(addresses)
-  # end
+  def test_that_clinic_coordinates_are_array
+  	addresses = @abortron.create_full_address(@abortron.clinics)
+  	assert_kind_of Array, @abortron.clinics_coordinates_conversion(addresses)
+  end
 
   def test_that_clinic_coordinates_are_found
   	addresses = [["1001 Broadway, Oakland, CA"], ["2430 Folsom, San Francisco, CA"], ["517 Castro, San Francisco, CA"]]
-  	obj = MiniTest::Mock.new
-		obj.expect :clinics_coordinates_conversion, [["37.8021736, -122.2729171"], ["37.758278,-122.415025"], ["37.7605162,-122.4347025"]], addresses
+  	ouput = [["37.8021736, -122.2729171"], ["37.758278,-122.415025"], ["37.7605162,-122.4347025"]]
+  	mock = MiniTest::Mock.new
+  	mock.expect(:ll, ["37.8021736, -122.2729171"])
+		Geokit::Geocoders::GoogleGeocoder.stub(:geocode, mock) do
+			@abortron.clinics_coordinates_conversion(addresses)
+		end
   end
 
-  # def test_that_address_coordinates_are_string
-  # 	addresses = @abortron.create_full_address(@abortron.clinics)
-  # 	addresses.each do |address|
-  # 		address.each do |coordinates|
-  # 			@coordinates = @abortron.clinics_coordinates_conversion(coordinates)
-  # 			p @coordinates
-  # 		end
-  # 	end
-  # 	assert_kind_of String, @coordinates
-  # end
-
-  def test_that_coordinates_are_found_for_patient
+  def test_that_patient_coordinates_are_found
+		pt_address = "88 Colin P Kelly Jr St, San Francisco, CA"
+  	pt_mock = MiniTest::Mock.new
+  	pt_mock.expect(:ll, ["37.78226710000001, -122.3912479"])
+		Geokit::Geocoders::GoogleGeocoder.stub(:geocode, pt_mock) do
+			@abortron.patient_coordinates_conversion(pt_address)
+		end
   end
 
   def test_that_distances_calculated_between_clinics_and_patient
